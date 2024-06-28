dc7980bf633b50d30f93fc9ef59a522b0d45b8c3
mrx_sandy <sandipgadekar143@gmail.com>
2024-06-28 15:54:48 +0530

    update all

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='hero'>
+
+<div>
+    <h1>YOUR FEET DESERVE THE BEST</h1>
+</div>
+<div>Image</div>
+
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+         <nav>
+        <h1>BrandLogo</h1>
+
+        <div className='logo'>
+          {/* <img src="" alt="logo" /> */}
+        </div>
+        <ul>
+        <li hrrf="#" >Home</li>
+
+          <li hrrf="#" >Menu</li>
+          <li hrrf="#" >About</li>
+          <li hrrf="#" >Contact</li>
+
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
